83934e24334d4c8cfeaaf1cc639a42f30efb430c
Fares-Esam783 <esamf401@gmail.com>
2025-12-10 17:27:00 +0200

    change_url

--- azkar/urls.py
+++ azkar/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import all_dhikr, category_list, dhikr_by_category, dhikr_detail, add_dhikr, edit_dhikr, delete_dhikr
 
 urlpatterns = [
-  path('', all_dhikr, name='all_dhikr'),
+  path('', category_list, name='category_list'),
   path('categories/', category_list, name='category_list'),
   path('category/<int:cat_id>/', dhikr_by_category, name='dhikr_by_category'),
   path('detail/<int:dhikr_id>/', dhikr_detail, name='dhikr_detail'),
