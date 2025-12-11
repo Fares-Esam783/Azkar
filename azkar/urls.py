93bc77fc8ec9c153f6ce69718a6eee92fa10e187
Fares-Esam783 <esamf401@gmail.com>
2025-12-11 16:34:34 +0200

    changes_update

--- azkar/urls.py
+++ azkar/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from .views import all_dhikr, category_list, dhikr_by_category, dhikr_detail, add_dhikr, edit_dhikr, delete_dhikr
 
 urlpatterns = [
-  path('', all_dhikr, name='all_dhikr'),
-  path('categories/', category_list, name='category_list'),
+  path('', category_list, name='category_list'),
+  path('all_dhikr/', all_dhikr, name='all_dhikr'),
   path('category/<int:cat_id>/', dhikr_by_category, name='dhikr_by_category'),
   path('detail/<int:dhikr_id>/', dhikr_detail, name='dhikr_detail'),
   path('add/', add_dhikr, name='add_dhikr'),
